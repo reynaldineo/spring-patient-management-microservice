fc82279ba109b0b3f765d3ecb2b4565cbb3dd788
reynaldi neo <reynaldineoramadhani@gmail.com>
2025-06-14 02:38:37 +0700

    feat(analytics): add kafka consumer

--- analytics-service/src/test/java/com/reynaldineo/analytics_service/AnalyticsServiceApplicationTests.java
+++ analytics-service/src/test/java/com/reynaldineo/analytics_service/AnalyticsServiceApplicationTests.java
@@ -2,12 +2,15 @@ package com.reynaldineo.analytics_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application.properties")
 class AnalyticsServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// Test that the application context loads without Kafka
 	}
 
 }
